166862847a1d03c4a5b20615e8ec5f20128a378b
Josh Sommer <joshdsommer@gmail.com>
2021-10-05 17:11:31 -0400

    adding components for searching and search results

---
+++ libs/search/src/lib/search-results/search-results.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'github-search-search-results',
+  templateUrl: './search-results.component.html',
+  styleUrls: ['./search-results.component.scss'],
+})
+export class SearchResultsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
