181e040263c500c479cb3d8e622fd91fbf532d60
Josh Sommer <joshdsommer@gmail.com>
2021-10-06 18:45:49 -0400

    basic wiring up of service to component.

--- libs/search/src/lib/search-results/search-results.component.ts
+++ libs/search/src/lib/search-results/search-results.component.ts
@@ -1,12 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { SearchResults } from './search-results.model';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'github-search-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.scss'],
 })
-export class SearchResultsComponent implements OnInit {
-  constructor() {}
+export class SearchResultsComponent
+  implements OnInit, OnDestroy, AfterViewInit
+{
+  results$: Observable<SearchResults> | undefined;
+  subscription: Subscription | undefined;
+  constructor(
+    private userSearch: UsersService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.results$ = this.userSearch.searchResults();
+  }
+
+  ngAfterViewInit() {
+    this.subscription = this.activatedRoute.queryParams
+      .pipe(
+        tap((queryParams) =>
+          this.userSearch.search(queryParams?.query, queryParams?.page || 1)
+        )
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
