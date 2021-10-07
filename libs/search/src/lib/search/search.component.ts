5f6bcc72b3f748925228b92cb0ed68a328a492fc
Josh Sommer <joshdsommer@gmail.com>
2021-10-06 22:11:52 -0400

    adding search box to the main search page

--- libs/search/src/lib/search/search.component.ts
+++ libs/search/src/lib/search/search.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'github-search-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent implements OnInit {
-  constructor() {}
+export class SearchComponent {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
-  ngOnInit() {}
+  queryChange(query: string) {
+    this.router.navigate(['results'], {
+      relativeTo: this.activatedRoute,
+      queryParams: { query, page: 1 },
+    });
+  }
 }
